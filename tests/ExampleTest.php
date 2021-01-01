b3944952fa7e629d95cc258547bc6aa392620939
Darren Coutts <couttsdarren@outlook.com>
2021-01-01 16:02:38 +0000

    Initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppKit\:package_name_php\Tests;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function trueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
